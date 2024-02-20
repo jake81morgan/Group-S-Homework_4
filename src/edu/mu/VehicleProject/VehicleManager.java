c53fd074f444565f8b4a40818495ffff06742f08
Jackson Morgan <jake81morgan@gmail.com>
2024-02-20 11:31:04 -0600

    changed Vehicle
    added attributes and methods to class and created VehicleManager class

---
+++ src/edu/mu/VehicleProject/VehicleManager.java
@@ -0,0 +1,5 @@
+package edu.mu.VehicleProject;
+
+public class VehicleManager {
+
+}
--- src/edu/mu/VehicleProject/Vehicles/Vehicle.java
+++ src/edu/mu/VehicleProject/Vehicles/Vehicle.java
@@ -1,5 +1,30 @@
 package edu.mu.VehicleProject.Vehicles;
 
-public class Vehicle {
+import edu.mu.VehicleProject.enums.*;
+
+public abstract class Vehicle {
+	
+	// Protected attributes
+	protected String brand;
+	protected String make;
+	protected long modelYear;
+	protected double price;
+	protected VehicleColor color;
+	protected FuelType fuelType;
+	protected double mileage;
+	protected double mass;
+	protected int cylinders;
+	protected double gasTankCapacity;
+	protected StartMechanism startType;
+	
+	// Abstract methods for implementation and overriding
+	public abstract double calculateMaintenaceCost(double distance);
+	public abstract double calculateFuelEfficiency(double distance, double fuelPrice);
+	public abstract void startEngine();
+	
+	
+
+	
+	
 
 }
