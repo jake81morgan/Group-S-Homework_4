b39fbd39496195bd36d04063f3f46dfebfec5c19
Jackson Morgan <jake81morgan@gmail.com>
2024-02-23 14:31:06 -0600

    Update Main.java

--- src/edu/mu/VehicleProject/Main.java
+++ src/edu/mu/VehicleProject/Main.java
@@ -1,10 +1,20 @@
 package edu.mu.VehicleProject;
 
+import java.util.ArrayList;
+
+import edu.mu.VehicleProject.Vehicles.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		VehicleManager manager = new VehicleManager();
+		manager.initializeStock();
+		ArrayList<Vehicle> vehicles = manager.getInventory();
+		for(Vehicle vehicle : vehicles) {
+			System.out.println(vehicle);
+		}
+		
 	}
 
 }
