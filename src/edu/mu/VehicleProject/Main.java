e39db34cfad91eb91ffa8b9fa7b03cbcae7a0778
Jackson Morgan <jake81morgan@gmail.com>
2024-02-24 20:57:35 -0600

    Update Main.java
    added test cases for all other methods

--- src/edu/mu/VehicleProject/Main.java
+++ src/edu/mu/VehicleProject/Main.java
@@ -3,11 +3,15 @@ package edu.mu.VehicleProject;
 import java.util.ArrayList;
 
 import edu.mu.VehicleProject.Vehicles.*;
+import edu.mu.VehicleProject.enums.FuelType;
+import edu.mu.VehicleProject.enums.StartMechanism;
+import edu.mu.VehicleProject.enums.VehicleColor;
 
 public class Main {
 	
 
 	public static void main(String[] args) {
+		// Creates instance of class
 		VehicleManager manager = VehicleManager.getInstance();
 
         // Initialize stock from file
@@ -39,8 +43,13 @@ public class Main {
 			}
 		}
 		manager.displayVehicleInformation(testcar);
-        // Remove a vehicle (replace "vehicleToRemove" with the actual vehicle object)
-        // boolean removed = manager.removeVehicle(vehicleToRemove);
+        
+		// Remove vehicle from inventory
+		manager.removeVehicle(testcar);
+		
+		// Add vehicle to inventory
+		Car car = new Car("Lincoln", "MKS", 2009, 10000, VehicleColor.GRAY, FuelType.GASOLINE, 192000, 2, 6, 17, StartMechanism.KEYSTART);
+		manager.addVehicle(car);
 
         // Save vehicle list to file
         boolean saved = manager.saveVehicleList();
@@ -49,5 +58,23 @@ public class Main {
         } else {
             System.out.println("\nFailed to save vehicle list.");
         }
+        
+        // Check number of cars
+        int cars = manager.getNumberOfVehiclesByType(Car.class);
+        System.out.println("\nNumber of cars: " + cars);
+        
+        // Tests get vehicle methods
+        System.out.println("\nVehicle with lowest maintenence cost: " + manager.getVehicleWithLowestMaintenanceCost(1000));
+        System.out.println("Vehicle with highest maintenence cost: " + manager.getVehicleWithHighestMaintenanceCost(1000));
+        System.out.println("\nVehicle(s) with lowest fuel efficiency: ");
+        for(Vehicle v : manager.getVehicleWithLowestFuelEfficiency(1000, 3.2)) {
+        	System.out.println("   " + v);
+        }
+        System.out.println("\nVehicle(s) with highest fuel efficiency: ");
+        for(Vehicle v : manager.getVehicleWithHighestFuelEfficiency(1000, 3.2)) {
+        	System.out.println("   " + v);
+        }
+        
+        System.out.println("\nAverage fuel efficiency of SUVs: " + manager.getAverageFuelEfficiencyOfSUVs(1000, 3.2));
 	}
 }
